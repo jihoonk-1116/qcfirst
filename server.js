136e9ac81b02116c89d5b44ccbb296c29f1a2231
jihoonk1116 <>
2021-04-23 05:07:00 +0000

    modify

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-const http = require('http');
+/*const http = require('http');
 const server = http.createServer((req, res)=>{
   res.writeHead(200);
   res.end('ok');
 });
 server.listen(3000);
+*/
+
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname))
+var server = app.listen(3000, () => {
+    console.log('server is listening on port', server.address().port)
+})
