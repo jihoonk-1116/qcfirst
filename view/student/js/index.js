348dfed6c4b24737a290180091a08e610aa2ffca
jihoonk-1116 <76544061+jihoonk-1116@users.noreply.github.com>
2021-05-24 14:51:03 -0400

    Delete view directory

--- view/student/js/index.js
+++
@@ -1,8 +0,0 @@
-// $(function () {
-//     $("#stu").click(function () {
-//         $("#login-btn").attr("href","https://ganiasif98.github.io/Final/student_overview.html")
-//     });
-//     $("#inst").click(function () {
-//         $("#login-btn").attr("href","https://jihoonk-1116.github.io/finalProject/overview.html")
-//     });
-// });
